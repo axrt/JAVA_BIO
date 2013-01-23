c5eaae23d61dab0199cdc87de3d1ef5c214f57c7
Alexander Tuzhikov <alexander.tuzhikov@gmail.com>
2013-01-23 11:24:39 +0400

    NCBI_Q_BLAST_EscapeSymbols string reformat method added that maintains legal symbols within the QBLAST command string

--- JAVA_BIO/src/BLAST/NCBI/remote/NCBI_Q_BLAST_EscapeSymbols.java
+++ JAVA_BIO/src/BLAST/NCBI/remote/NCBI_Q_BLAST_EscapeSymbols.java
@@ -7,12 +7,12 @@ package BLAST.NCBI.remote;
  *         consult <a href=
  *         "http://www.ncbi.nlm.nih.gov/staff/tao/URLAPI/new/node101.html#sub:Escape-of-Unsafe"
  *         >this</a> and <a
- *         href="http://www.google.ru/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ve
- *         d =0CFYQFjAA&url
+ *         href="http://www.google.ru/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&v
+ *         e d =0CFYQFjAA&url
  *         =http%3A%2F%2Falextblog.blogspot.com%2F2012%2F05%2Fncbi-blast-jaxb
  *         -biojava-blasting
  *         -like.html&ei=PjrkT5WFIInm-gaMtb2OCg&usg=AFQjCNF3uh28ITE1urA7wVPYGkHRPdmO_Q
- *         &sig2=Kq_j1KT32l1ERD-NTtkOkQ">this</a> for more details.
+ *         &sig2=Kq_j1KT32l1ERD-NTtkOkQ">this</a>.
  */
 public class NCBI_Q_BLAST_EscapeSymbols {
 	/**
@@ -51,4 +51,34 @@ public class NCBI_Q_BLAST_EscapeSymbols {
 	 * The '#' symbol is escaped with "%23"
 	 */
 	public static final String numberSign = "%23";
+
+	/**
+	 * Reformats all QBLAST illegal characters for legal substitution
+	 * characters. For more detail please consult <a href=
+	 * "http://www.ncbi.nlm.nih.gov/staff/tao/URLAPI/new/node101.html#sub:Escape-of-Unsafe"
+	 * >NCBI documentation.</a><br>
+	 * 
+	 * @param toReformat
+	 *            a {@link String} to format
+	 * @return a reformatted {@link String} ready for a QBLAST task request.
+	 */
+	public static String reformatForRequest(String toReformat) {
+		toReformat = toReformat.replaceAll(String.valueOf('%'),
+				NCBI_Q_BLAST_EscapeSymbols.percent);
+		toReformat = toReformat.replaceAll("\\[",
+				NCBI_Q_BLAST_EscapeSymbols.leftSquareBracket);
+		toReformat = toReformat.replaceAll("\\]",
+				NCBI_Q_BLAST_EscapeSymbols.rightSquareBracket);
+		toReformat = toReformat.replaceAll(String.valueOf(' '),
+				String.valueOf(NCBI_Q_BLAST_EscapeSymbols.whiteSpace));
+		toReformat = toReformat.replaceAll(String.valueOf('\n'),
+				NCBI_Q_BLAST_EscapeSymbols.newLine);
+		toReformat = toReformat.replaceAll(String.valueOf('#'),
+				NCBI_Q_BLAST_EscapeSymbols.numberSign);
+		toReformat = toReformat.replaceAll(String.valueOf('@'),
+				NCBI_Q_BLAST_EscapeSymbols.atSign);
+		toReformat = toReformat.replaceAll("\\|",
+				NCBI_Q_BLAST_EscapeSymbols.verticalSlash);
+		return toReformat;
+	}
 }
