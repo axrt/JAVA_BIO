bb328d6c99e08a0d813af35ed7794c80736257e8
Alexander Tuzhikov <alexander.tuzhikov@gmail.com>
2013-01-23 23:17:44 +0400

    BadFromatException introduced

---
+++ JAVA_BIO/src/format/BadFromatException.java
@@ -0,0 +1,24 @@
+package format;
+
+public abstract class BadFromatException extends Exception {
+
+	public BadFromatException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadFromatException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadFromatException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadFromatException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
